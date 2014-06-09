d45dc5c8c68bf3dd9ec69fef90ff3c3c13819309
Irl Nathan <irlnathan@gmail.com>
2014-06-09 18:02:09 -0500

    added better comments to Generator.js

--- Generator.js
+++ Generator.js
@@ -143,6 +143,16 @@ module.exports = {
       compiledShowFormFields: compiledShowFormFields
     }); 
 
+    // This generates a template using the actionParamObject.template located in spar/templates
+    // to produce a the params to include.
+    var ACTION_PARAM_OBJECT_TEMPLATE = path.resolve(__dirname, './templates/actionParamObject.template');
+    ACTION_PARAM_OBJECT_TEMPLATE = fs.readFileSync(ACTION_PARAM_OBJECT_TEMPLATE, 'utf8');
+
+    var compliledActionParamObject = _.template(ACTION_PARAM_OBJECT_TEMPLATE, {
+        modelAttributeNames: scope.modelAttributeNames,
+        id: scope.id,
+        modelControllerName: scope.modelControllerName
+    })    
     
     // This generates a template using the action.template located in spar/templates
     // combined with CRUD actions to produce a controller.
